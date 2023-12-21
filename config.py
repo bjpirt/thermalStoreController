d49a38c28b57a170c01f536684a7f21d51d778cd
Ben Pirt <ben@pirt.co.uk>
2023-12-21 09:48:13 +0000

    Refactor persistent config into a parent class

--- config.py
+++ config.py
@@ -1,19 +1,9 @@
-import os
-import json
+from persistent_config import PersistentConfig
 from initial_config import config
 
 
-def exists(filename: str) -> bool:
-    try:
-        os.stat(filename)
-        return True
-    except OSError:
-        return False
-
-
-class Config:
-    def __init__(self, file="config.json"):
-        self.__file = file
+class Config(PersistentConfig):
+    def __init__(self):
         # Initialise the defaults
         self.wifi_network = "WIFI_NETWORK"
         self.wifi_password = 'WIFI_PASSWORD'
@@ -29,36 +19,4 @@ class Config:
         self.outside_temperature_low_setpoint = 12
         self.occupied_ac_power = 300
         self.occupied_time = 86400
-
-        self.update(config)
-
-        self.read()
-
-    def get_dict(self):
-        return {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
-
-    def read(self):
-        data = None
-        if exists(self.__file):
-            with open(self.__file, 'r', encoding="UTF-8") as file:
-                data = file.read()
-                if data:
-                    new_config = json.loads(data)
-                    self.update(new_config)
-
-    def set_value(self, key: str, value):
-        if hasattr(self, key):
-            if type(getattr(self, key)) == type(value) or type(value) == type(None):
-                setattr(self, key, value)
-            else:
-                print(f"Config types did not match: {key} ({type(getattr(self, key))}) ({type(value)})")
-        else:
-            print("Attribute does not exist")
-
-    def update(self, new_config: dict) -> None:
-        for (key, value) in new_config.items():
-            self.set_value(key, value)
-
-    def save(self):
-        with open(self.__file, 'w', encoding="utf-8") as file:
-            json.dump(self.get_dict(), file)
+        super().__init__(config)
